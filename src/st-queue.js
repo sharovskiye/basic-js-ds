84d9287076efe278bc00051ecf6b5c98c0b5cdb0
Yevhenii <sharovskiyevgeniy@gmail.com>
2021-10-15 19:38:22 +0300

    feat: complete 16/18 test

--- src/st-queue.js
+++ src/st-queue.js
@@ -13,21 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+
+class Node{
+  constructor(value){
+    this.value = value
+    this.next = null
+  }
+}
+
 module.exports = class Queue {
+
+  constructor(){
+    this.head=null
+    this.tail=null
+    this.length=0
+  }
 
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    let node=new Node(value)
+
+    if(this.head){
+      this.tail.next=node
+      this.tail=node
+    } else{
+      this.head=node
+      this.tail=node
+
+    }
+    this.length++
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+ let current=this.head
+ this.head=this.head.next
+ this.length--
+ return current.value
   }
 
 }
